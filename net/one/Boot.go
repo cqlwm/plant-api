30008f8738ff09dae6f80ff8e18efce6b335faf2
cqlwm <cqlwm@outlook.com>
2021-04-27 14:39:40 +0800

    main.go

---
+++ net/one/Boot.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"plant-api/net/one/config"
+	"plant-api/net/one/handler"
+	"plant-api/net/one/handler/article"
+	"plant-api/net/one/handler/tool"
+	"plant-api/net/one/handler/user"
+)
+
+func main() {
+	// 参数加载
+	config.OsLoad()
+
+	// GRom
+	ok, err := config.Init()
+	if err != nil {
+		fmt.Println("GRom 数据源 : ", err)
+		return
+	}
+	if ok {
+		fmt.Println("数据库初始化完成")
+	}
+
+	// Redis
+	err = config.InitRedisClient()
+	if err != nil {
+		fmt.Println("Redis 初始化 : ", err)
+		return
+	}
+
+	// MongoDB
+	config.MongoDBConn()
+
+	// Gin
+	r := config.R()
+	handler.TestHandlerRoute(r) // 测试
+	tool.DistinguishHandler(r)  // 虫、草、药识别
+	user.UserHandler(r)         // 用户
+	article.ArticleHandler(r)   // 文章
+
+	r.Run(config.Port)
+}
+
+func main2() {
+	//crawling.PestWebRequest()
+	//srcByte, err := ioutil.ReadFile(`C:\Users\lwm\Desktop\1\1.jpg`)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//res := base64.StdEncoding.EncodeToString(srcByte)
+	//
+	//fmt.Println(res)
+}
