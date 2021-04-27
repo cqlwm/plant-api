0408eb79d51eff0942ce7075d4c659d17bbb52d6
cqlwm <cqlwm@outlook.com>
2021-04-27 14:33:21 +0800

    完善常量配置

--- net/one/config/FinalConstant.go
+++ net/one/config/FinalConstant.go
@@ -1,24 +1,43 @@
 package config
 
-const (
-	//ReUrlImage = `http://127.0.0.1:8848/browser/image/`
-	//ImageSavePathConstant = `C:\Users\lwm\Desktop\demo-for-aliyun-plant-api-master\javascript\browser\image\`
+import "runtime"
 
-	SearchByImage = `F:\test\images\plant\searchby\`
+// 分词词库路径
+var SegmenterLoadDictionary = "/root/plant/staticfile/dictionary.txt"
 
-	// 生产环境
-	ImageSavePathConstant = `/usr/local/nginx/html/plant/image/item/`
-	ReUrlImage            = `https://graceful.top/plant/image/item/`
-	// 爬取每日最新资讯，图片保存路径；对应以图搜图的功能；
-	// SearchByImage = `/usr/local/nginx/html/plant/image/searchby/`
-	SearchByImageHttpUrl = `https://graceful.top/plant/image/searchby/`
+// 临时图片文件：本地、网络地址
+var ImageSavePathConstant = `/usr/local/nginx/html/plant/image/item/`
+var ReUrlImage = `https://graceful.top/plant/image/item/`
 
-	Port = ":9091"
+// 爬取每日最新资讯，图片保存路径；对应以图搜图的功能；
+var SearchByImage = `/usr/local/nginx/html/plant/image/searchby/`
+var SearchByImageHttpUrl = `https://graceful.top/plant/image/searchby/`
 
-	// MongoDB Key
-	WeedKey  = "WeedKey"
-	WeedColl = "weeds"
+// 端口号
+var Port = ":9091"
 
-	// 定时文章爬虫
-	ImageTag = "ThisImageTag:"
-)
+// ######################### MongoDB Key #########################
+// 杂草识别模块
+var WeedKey = "WeedKey"
+var WeedColl = "weeds"
+
+// 定时文章爬虫，存入数据库时的Tag
+var ImageTag = "ThisImageTag:"
+
+// ######################### Redis Key #########################
+// 文章搜索IDS
+var ArticleIds = "ArticleSearchId:"
+
+// Windows环境加载
+func OsLoad() {
+	osName := runtime.GOOS
+	winPath := `F:\deskFile\02智慧农业\plant`
+	if osName == "windows" {
+		// 临时图片放置
+		ImageSavePathConstant = winPath + `\test\`
+		// 搜索图片
+		SearchByImage = winPath + `\searchby\`
+		// 分词词库路径
+		SegmenterLoadDictionary = winPath + `\dictionary.txt`
+	}
+}
